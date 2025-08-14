5c04ecc9ee97c176110e76f440feb4ea005dde61
dyleo16 <dylanleo160312@gmail.com>
2025-08-14 11:28:30 -0600

    Mi Primer Commit en Java

---
+++ IF2000_IntroduccionProgra/src/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introduccionprogra;
+
+/**
+ *
+ * @author dylan
+ */
+public class IF2000_IntroduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    //Data types
+String name = "Lionel";
+int age = 40;
+
+System.out.println("Hello World");
+System.out.println("Name: " +name);
+System.out.println("Age: " +age);
+/*Tipos de datos numericos:
+  int, long, short,float, double
+  numeric, money, decimal
+
+  Manejo de fecha:
+  Date
+
+ Cadena de carecteres:
+  String
+
+Objectos: 
+Object
+
+Data Structures: 
+Arrays or Vectors: int[] x
+                   int[] y
+
+
+*/
+    }
+    
+}
